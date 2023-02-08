9f7a2191040f3b7b9ab64948d5097661fd502bb5
lubwama <bizzicole87@gmail.com>
2023-02-08 08:04:54 +0300

    Jarvis

--- components/NftDisplay.tsx
+++ components/NftDisplay.tsx
@@ -1,18 +1,21 @@
 import { JsonMetadata } from "@metaplex-foundation/js"
 
 interface NftProps {
-  json: JsonMetadata<string>
+  json: JsonMetadata<string> | null
 }
 
 export default function NftDisplay({ json }: NftProps) {
-  const certificate = (json.attributes?.find(a => a.trait_type === "Certificate") || {}).value || "";
+  if (!json) {
+    return <p>Holders only! ☠️</p>;
+  }
 
+  const certificate = (json.attributes?.find(a => a.trait_type === "Certificate") || {}).value || "";
 
   return (
     <div className="flex flex-col gap-4">
       <h3 className="text-lg font-medium text-white">{json.name}</h3>
       <img src={json.image} alt={json.name} />
-      <p className="text-sm text-white"><span className="font-bold">Arms: </span>{certificate}</p>
+      <p className="text-sm text-white"><span className="font-bold">Certificate: </span>{certificate}</p>
     </div>
-  )
-}
+  );
+};
