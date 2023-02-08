38892e237ef16fd99c9166327e18fedcaa9ef7d4
lubwama <bizzicole87@gmail.com>
2023-02-08 07:56:29 +0300

    Jarvis

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,66 +1,66 @@
-import { Nft, Metaplex } from "@metaplex-foundation/js";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { useEffect, useState } from "react";
-import NftDisplay from "../components/NftDisplay";
-import PageHeading from "../components/PageHeading";
-
-// The address of the collection's mint contract
-const COLLECTION_MINT_ADDRESS = "...";
+import { Metadata, Metaplex, Nft } from "@metaplex-foundation/js"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useEffect, useState } from "react"
+import NftDisplay from "../components/NftDisplay"
+import PageHeading from "../components/PageHeading"
+import { COLLECTION_MINT_ADDRESS } from "../lib/constants"
 
 export default function Holders() {
-  const { connection } = useConnection();
-  const wallet = useWallet();
-  const [userNfts, setUserNfts] = useState<Nft[]>([]);
+  const { connection } = useConnection()
+  const wallet = useWallet()
+  const [userNfts, setUserNfts] = useState<Nft[]>([])
 
-  // Instantiate the Metaplex instance
-  const metaplex = Metaplex.make(connection);
+  const metaplex = Metaplex
+    .make(connection)
 
-  // Get all NFTs from the Metaplex instance
-  const nfts = metaplex.nfts();
+  const nfts = metaplex.nfts()
 
   async function getUserNfts() {
     if (!wallet.publicKey) {
-      setUserNfts([]);
-      return;
+      setUserNfts([])
+      return
     }
 
-    // Fetch all the user's NFTs using the findAllByOwner function
-    const userNfts = await nfts.findAllByOwner({
-      owner: wallet.publicKey,
-    }) as Nft[];
+    // Fetch all the user's NFTs using the findAllByOwner function, which takes the owner public key as input and returns a list of objects describing the metadata of owned NFTs
+    const userNfts = await nfts
+      .findAllByOwner({ owner: wallet.publicKey })
 
     // Filter to our collection
     const ourCollectionNfts = userNfts.filter(
-      (metadata) =>
+      metadata =>
         metadata.collection !== null &&
         metadata.collection.verified &&
-        metadata.collection.address.toBase58() === COLLECTION_MINT_ADDRESS
-    );
+        metadata.collection.address.toBase58() === COLLECTION_MINT_ADDRESS.toBase58()
+    )
 
     // Load the JSON for each NFT
-    const loadedNfts = await Promise.all(
-      ourCollectionNfts.map((metadata) => {
-        return nfts.load({ metadata });
+    const loadedNfts = await Promise.all(ourCollectionNfts
+      .map(metadata => {
+        return nfts
+          .load({ metadata: metadata as Metadata })
       })
-    ) as Nft[];
+    )
 
-    console.log("Got their NFTs!", loadedNfts);
-    setUserNfts(loadedNfts);
+    console.log("Got their NFTs!", loadedNfts)
+    setUserNfts(loadedNfts as Nft[])
   }
 
   useEffect(() => {
-    getUserNfts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [wallet.publicKey]);
+    getUserNfts()
+  }, [wallet.publicKey])
 
   if (userNfts.length === 0) {
-    return <PageHeading>Holders only! ☠️</PageHeading>;
+    return (
+      <PageHeading>Holders only! ☠️</PageHeading>
+    )
   }
 
   return (
     <main className="flex flex-col gap-8">
       <PageHeading>Hello Autodidact!</PageHeading>
+
       <p className="text-lg text-white">Boon 🏆</p>
+
       <div className="grid grid-cols-2 gap-4">
         {userNfts.map((userNft, i) => (
           <NftDisplay key={i} json={userNft.json} />
