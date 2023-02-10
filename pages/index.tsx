0402308e61ab8d1bf2776e39c5ad63c1070b43c0
lubwama <bizzicole87@gmail.com>
2023-02-10 16:52:50 +0300

    updated quiz

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-8">
 
-      <Link href="/math">
+      {/* <Link href="/math">
       <button
           type="button"
           className="inline-flex items-center px-4 py-2 text-sm font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md cursor-pointer hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           Mathematics🔢
         </button> 
-        </Link>
+        </Link> */}
 
         <Link href="/history">
       <button
