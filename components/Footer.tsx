e676791dac4095b3b035c420c8334afd50ec4c55
lubwama <bizzicole87@gmail.com>
2023-02-10 17:21:53 +0300

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Footer() {
     return (
-      <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
+      <footer className="flex w-full justify-center border-t-2 border-gray-400 py-12 h-10  text-zinc-200">
         <Link 
          href='http://mwambazi-collins.vercel.app/'
          target='_blank'
