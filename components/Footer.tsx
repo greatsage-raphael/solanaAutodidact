386f8c605db32d52e64296b25a33b4aace235766
lubwama <bizzicole87@gmail.com>
2023-02-24 16:04:11 +0300

    vercel analytics

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,17 @@ export default function Footer() {
     return (
       <footer className="flex w-full justify-center gap-4 px-4 pt-4 pb-20 border-t-2 border-gray-400 py-12 h-20  text-zinc-200">
         <Link 
+         href='https://twitter.com/Autodid06724990'
+         target='_blank'
+         rel='noopener noreferrer'>
+           <button
+          type="button"
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-indigo-100 bg-indigo-700 border border-transparent rounded-md cursor-pointer hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed">
+          Twitter 🐦
+        </button>
+        </Link> 
+
+        <Link 
          href='http://mwambazi-collins.vercel.app/'
          target='_blank'
          rel='noopener noreferrer'>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
+import { Analytics } from '@vercel/analytics/react';
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
@@ -40,6 +41,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           </WalletModalProvider>
         </WalletProvider>
       </ConnectionProvider>
+      <Analytics />
     </>
   )
 }
