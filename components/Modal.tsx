e34cf5c24392eb15dfc02433783c398a0ab83e5a
lubwama <bizzicole87@gmail.com>
2023-03-06 23:54:35 +0300

    fixing jarvis' scroll feature

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -48,7 +48,7 @@ import Leaflet from "./Leaflet";
                   <motion.div
                     ref={desktopModalRef}
                     key="desktop-modal"
-                    className="fixed inset-0 z-40 hidden min-h-screen items-center justify-center md:flex"
+                    className="overflow-y-auto fixed inset-0 z-40 hidden min-h-screen items-center justify-center md:flex"
                     initial={{ scale: 0.95 }}
                     animate={{ scale: 1 }}
                     exit={{ scale: 0.95 }}
@@ -63,7 +63,7 @@ import Leaflet from "./Leaflet";
                 </FocusTrap>
                 <motion.div
                   key="desktop-backdrop"
-                  className="fixed inset-0 z-30 bg-gray-100 bg-opacity-10 backdrop-blur"
+                  className="overflow-y-auto fixed inset-0 z-30 bg-gray-100 bg-opacity-10 backdrop-blur"
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
